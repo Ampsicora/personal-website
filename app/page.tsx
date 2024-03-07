6e12bf73697e771a59ceebfff7e8d61a67a42c6e
Alessandro Sartori <alessandro@sartori.io>
2024-03-08 00:13:11 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-zinc-500 md:text-2xl">
-					✨ Software Engineer & Technical Analyst
+					✨ Software Engineer & Technical Analyst 🔮
 				</h2>
 			</div>
 			{/* Socials */}
